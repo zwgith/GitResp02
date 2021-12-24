18263a02fa87e1742325c7e4b1446a7075660de2
zw <zw@126.com>
2021-12-24 15:40:57 +0800

    first

---
+++ test01/src/com/mk/pojo/Student.java
@@ -0,0 +1,11 @@
+package com.mk.pojo;
+
+/**
+ * @Auther: ***
+ * @Date: 2021/12/24 - 12 - 24 - 15:37
+ * @Description: com.mk.pojo
+ * @version: 1.0
+ */
+public class Student {
+    private String name;
+}
